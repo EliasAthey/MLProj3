8bba90954a925ecaeb28b8d092591fe064c83c71
Elias A <athey.elias@gmail.com>
2017-11-04 23:10:42 -0600

    Added resources folder conatining datasets, referenced correctly by Data classes

--- NeuralNetworkTrainer/src/neuralNetworkTrainer/Data.java
+++ NeuralNetworkTrainer/src/neuralNetworkTrainer/Data.java
@@ -1,7 +1,22 @@
 package neuralNetworkTrainer;
 
+import java.util.ArrayList;
+
 /**
  * A parent class for all data classes
  */
 public abstract class Data {
+
+    /**
+     * The data points
+     */
+    protected ArrayList<ArrayList<Object>> dataPoints;
+
+    /**
+     * Gets the data points for this set of data
+     * @return the data points for this set of data
+     */
+    public ArrayList<ArrayList<Object>> getDataPoints() {
+        return dataPoints;
+    }
 }
--- NeuralNetworkTrainer/src/neuralNetworkTrainer/MachineData.java
+++ NeuralNetworkTrainer/src/neuralNetworkTrainer/MachineData.java
@@ -1,9 +1,5 @@
 package neuralNetworkTrainer;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
 import java.util.ArrayList;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
@@ -11,49 +7,32 @@ import java.util.Scanner;
 public class MachineData extends Data{
 
     /**
-     * The path to the machine datafile
-     */
-    private final String filepath = "src/datasets/machine.data";
-
-    /**
-     * The machine datafile
-     */
-    private final File dataFile;
-
-    /**
-     * The data points
-     */
-    private ArrayList<ArrayList<Integer>> dataPoints = new ArrayList<>();
-
-    /**
      * Construct MachineData
      */
     MachineData(){
-        this.dataFile = new File(this.filepath);
-        if(!dataFile.exists()){
-            System.out.println("Error: " + this.filepath + " does not exist.\n");
-            System.exit(0);
-        }
+        super.dataPoints = new ArrayList<>();
         this.setDataPoints();
     }
 
+    /**
+     * Sets the data points by scanning the file
+     */
     private void setDataPoints(){
-        try{
-            Scanner fileScanner = new Scanner(this.dataFile);
-            do{
-                try{
-                    String line = fileScanner.nextLine();
-                    System.out.println(line);
+        Scanner fileScanner = new Scanner(Thread.currentThread().getContextClassLoader().getResourceAsStream("machine.data"));
+        do{
+            try{
+                String line = fileScanner.nextLine();
+                String[] entries = line.split(",");
+                ArrayList<Object> values = new ArrayList<>();
+                for(String entry : entries){
+                    values.add(Integer.parseInt(entry));
                 }
-                catch(NoSuchElementException e){
-                    break;
-                }
+                super.dataPoints.add(values);
             }
-            while(true);
+            catch(NoSuchElementException e){
+                break;
+            }
         }
-        catch(IOException e){
-            System.out.println(e.getMessage());
-            System.exit(0);
-        }
+        while(true);
     }
 }
