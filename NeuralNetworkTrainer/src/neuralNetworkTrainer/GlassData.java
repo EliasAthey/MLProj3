bcfb54abb9889b6b884355328f366d7cbb8dbed4
Elias A <athey.elias@gmail.com>
2017-11-05 12:46:49 -0700

    Implemented glass data

--- NeuralNetworkTrainer/src/neuralNetworkTrainer/GlassData.java
+++ NeuralNetworkTrainer/src/neuralNetworkTrainer/GlassData.java
@@ -1,4 +1,40 @@
 package neuralNetworkTrainer;
 
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 public class GlassData extends Data{
+
+    /**
+     * Constructs GlassData
+     */
+    GlassData(){
+        numInputs = 9;
+        numOutputs = 1;
+        dataPoints = new ArrayList<>();
+        this.setDataPoints();
+    }
+
+    /**
+     * Sets the data points by scanning the file
+     */
+    private void setDataPoints(){
+        Scanner fileScanner = new Scanner(Thread.currentThread().getContextClassLoader().getResourceAsStream("glass.data"));
+        do{
+            try{
+                String line = fileScanner.nextLine();
+                String[] entries = line.split(",");
+                ArrayList<Object> values = new ArrayList<>();
+                for(String entry : entries){
+                    values.add(Double.parseDouble(entry));
+                }
+                dataPoints.add(values);
+            }
+            catch(NoSuchElementException e){
+                break;
+            }
+        }
+        while(true);
+    }
 }
